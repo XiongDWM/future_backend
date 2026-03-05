e7b60b55be2fbcef52929abba188123dd9eb0b0f
XiongDWM <745887012@qq.com>
2026-03-06 01:20:01 +0800

    feat(working_log): define working log class

--- src/main/java/com/xiongdwm/future_backend/entity/WorkingTimeLog.java
+++ src/main/java/com/xiongdwm/future_backend/entity/WorkingTimeLog.java
@@ -3,11 +3,14 @@ package com.xiongdwm.future_backend.entity;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,7 +21,7 @@ public class WorkingTimeLog {
     private Long id;
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     @Column
-    private Date date; // 工作日期 如果是上午 回退一天的日期 如果是 下午则就是当天日期
+    private Date date; // 工作日期 如果是0-16 回退一天的日期 如果是16-23则就是当天日期
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @Column
     private Date thisTermBeginTime;
@@ -27,5 +30,8 @@ public class WorkingTimeLog {
     private Date thisTermEndTime;
     @Column
     private double timeLasted; // 本次工作时长，单位小时
-
+    @ManyToOne
+    @JoinColumn(name = "user_id_working_log", referencedColumnName = "id")
+    @JsonManagedReference("user-working-logs")
+    private User user; // 关联用户
 }
