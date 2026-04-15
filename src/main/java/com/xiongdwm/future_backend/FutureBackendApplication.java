b6d30389e79b9e68dcc8b810e28fbaf6f9bdd258
XiongDWM <745887012@qq.com>
2026-04-15 14:30:41 +0800

    chore(thread&gc): change gc type to zgc and enable virtual thread

--- src/main/java/com/xiongdwm/future_backend/FutureBackendApplication.java
+++ src/main/java/com/xiongdwm/future_backend/FutureBackendApplication.java
@@ -12,9 +12,13 @@ import org.springframework.boot.jdbc.autoconfigure.DataSourceInitializationAutoC
 	DataSourceInitializationAutoConfiguration.class
 })
 public class FutureBackendApplication {
-
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(FutureBackendApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(FutureBackendApplication.class, args);
+
+		// 打印 GC 类型
+		String gcName = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans().getFirst().getName();
+		logger.info("[GC] Using " + gcName);
 	}
 
 }
